9c4004b68424e00683017709f5d0f72cf48b400b
ke.wang@samesamechina.com <ke.wang@samesamechina.com>
2017-09-05 18:07:07 +0800

    en:init project

---
+++ bundle/CampaignBundle/PageController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace CampaignBundle;
+
+use Core\Controller;
+
+class PageController extends Controller
+{
+	public function indexAction() {
+		global $user;
+		var_dump($user);exit;
+		$RedisAPI = new \Lib\RedisAPI();
+		$config = $RedisAPI->jssdkConfig($this->request->getUrl(TRUE));
+		return $this->render('index', array('config' => $config));
+	}
+
+	public function clearCookieAction() {
+      	$request = $this->Request();
+		setcookie('_user', '', time(), '/', $request->getDomain());
+		$this->statusPrint('success');
+	}
+
+
+
+}
